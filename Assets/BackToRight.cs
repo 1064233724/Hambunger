310e22c686820e0bad917cfe07a79351a4e93751
edeng23 <edencgaon@gmail.com>
2015-09-24 18:42:27 +0300

    pepper

---
+++ Assets/BackToRight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToRight : MonoBehaviour {
+	
+	int numBGPanels = 6;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.tag == "BG") {
+			//Debug.Log ("Triggered: " + collider.name);
+			
+			float WidthOfBGObject = ((BoxCollider2D)collider).size.x;
+			//Debug.Log(HeightOfBGObject);
+
+			Vector3 pos = collider.transform.position;
+			pos.x += WidthOfBGObject * numBGPanels;
+			
+			collider.transform.position = pos;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Hover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+
+	Rigidbody2D rg2d;
+	bool tof = true;
+	int counter = 0;
+	// Use this for initialization
+	void Start () {
+		rg2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tof == true) {
+			counter ++;
+			Vector3 temp = rg2d.position;
+			temp.y -= 0.003f;
+			rg2d.transform.position = temp;
+			if (counter == 150)
+			{
+				tof = false;
+				counter =0;
+			}
+		} 
+		else {
+			counter++;
+			Vector3 temp = rg2d.position;
+			temp.y += 0.003f;
+			rg2d.transform.position = temp;
+			if (counter == 150)
+			{
+				tof = true;
+				counter =0;
+			}
+		}
+	}
+}
