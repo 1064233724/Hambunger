6dcd6ef48591c4f7d2de0c873b1a9ca4609911ce
edeng23 <edencgaon@gmail.com>
2015-09-24 17:26:20 +0300

    intial

---
+++ Assets/MusBackMov.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusBackMov : MonoBehaviour {
+	
+	Rigidbody2D rg2d;
+	
+	// Use this for initialization
+	void Start () {
+		rg2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = rg2d.position;
+		temp.x -= 0.03f	;
+		rg2d.transform.position = temp;
+	}
+}
